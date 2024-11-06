92cdb37ed5a8d132ae92ab9a53707e7531c1823e
zkv0 <rdroxhk5@duck.com>
2024-11-05 21:26:13 -0500

    cambios menores

--- js/script.js
+++ js/script.js
@@ -11,4 +11,23 @@ const rotacionEvento = ['click','touchstart',]
 for (let i = 0; i < rotacionEvento.length; i++) {
     const elementValor = rotacionEvento[i];
     miFotoimg.addEventListener(elementValor, alternarRotacionImg)
+};
+
+
+
+
+const enviarcorreo = document.querySelectorAll('.enviarcorreo')
+
+
+for (let i = 0; i < enviarcorreo.length; i++) {
+    const element = enviarcorreo[i];
+    element.addEventListener('click', function () {
+        const micorreo = "2dz1m2c06s@proton.me";
+        const miasunto = 'Estamos interesados en ti.';
+    
+        const enlacemicorreo = `mailto:${micorreo}?subject=${miasunto}`;
+          // Redirigimos al usuario al enlace mailto
+          window.location.href = enlacemicorreo;
+    });
+    
 }
