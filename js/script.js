0c3e34d56dae1a963e3f5c7144206079743e4594
zkv0 <rdroxhk5@duck.com>
2024-11-06 19:48:50 -0500

    pdf view añadido, ademas se almacena el valor del estado de rotacion de la imagen stop en el localstore

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
 const miFotoimg = document.querySelector('#mifotoimg');
 
+let estadoDeRotacion = JSON.parse(localStorage.getItem('miEstadoRotacion')) || false;
+
+
 function alternarRotacionImg() {
-    if (miFotoimg.style.animationPlayState === 'paused') {
+    if (estadoDeRotacion === false) {
         miFotoimg.style.animationPlayState = 'running';
-    }   else { miFotoimg.style.animationPlayState = 'paused'; }
+        estadoDeRotacion = true;
+    }   else { miFotoimg.style.animationPlayState = 'paused';
+        estadoDeRotacion = false;  // cambiamos el estado de la rotación al contrario del estado anterior
+     }
+     // Guardar en localStorage el estado
+    localStorage.setItem('miEstadoRotacion', JSON.stringify(estadoDeRotacion));
 }
 
-const rotacionEvento = ['click','touchstart',]
 
-for (let i = 0; i < rotacionEvento.length; i++) {
-    const elementValor = rotacionEvento[i];
-    miFotoimg.addEventListener(elementValor, alternarRotacionImg)
-};
+
+miFotoimg.addEventListener('click', alternarRotacionImg);
+
+// Aplicar el estado inicial de la animación basado en el valor de 'estadoDeRotacion'
+if (estadoDeRotacion === true) {
+    miFotoimg.style.animationPlayState = 'running';
+} else {
+    miFotoimg.style.animationPlayState = 'paused';
+}
+
 
 
 
@@ -21,6 +34,7 @@ const enviarcorreo = document.querySelectorAll('.enviarcorreo');
 
 for (let i = 0; i < enviarcorreo.length; i++) {
     const element = enviarcorreo[i];
+
     element.addEventListener('click', function () {
         const micorreo = "2dz1m2c06s@proton.me";
         const miasunto = 'Estamos interesados en ti.';
@@ -31,3 +45,25 @@ for (let i = 0; i < enviarcorreo.length; i++) {
     });
     
 }
+
+
+//traemos todos los botones que nos redirijen al visor de la hoja de vida
+
+const abrirHdv = document.querySelectorAll('.abrirHdv');
+
+// agregamos un evento de click a cada botón para abrir el visor de la hoja de vida
+abrirHdv.forEach(function(buttonHdv){
+    buttonHdv.addEventListener('click', function() {
+        window.location.href = "mis-documentos/pdf-visor/web/viewer.html?file=../../mi-hdv.pdf"
+    });
+});
+
+
+const linkMiGit = document.querySelectorAll('.linkMiGit');
+
+// agregamos un evento de click a cada botón para abrir el visor de la hoja de vida
+linkMiGit.forEach(function(buttonMiGit){
+    buttonMiGit.addEventListener('click', function() {
+        window.open("https://github.com/zkv0");
+    });
+});
